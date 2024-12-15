f509b897bf0ce4bff5564fbb5988a01464dee0b0
GregorSchulze <gr.trash@t-online.de>
2024-12-15 18:45:48 +0100

    Update
    Verbesserung der Lesbarkeit des Codes für die Filterfunktion.

--- js/searchFilter.js
+++ js/searchFilter.js
@@ -1,5 +1,6 @@
+// Funktion für die Filterung
 const handleSearch = (event, elements) => {
-  const searchTerm = event.taret.value.toLowerCase();
+  const searchTerm = event.target.value.toLowerCase();
   elements.forEach((element) => {
     const caption = element.getAttribute("data-caption").toLowerCase();
     element.style.display =
@@ -7,27 +8,16 @@ const handleSearch = (event, elements) => {
   });
 };
 
-// // Funktion für die Filterung
-// const handleSearch = (event, elements) => {
-//   const searchTerm = event.target.value.toLowerCase();
-//   elements.forEach((element) => {
-//     const caption = element.getAttribute("data-caption").toLowerCase();
-//     element.style.display =
-//       caption.includes(searchTerm) || !searchTerm ? "" : "none";
-//   });
-// };
+// Filter-Klasse
+class Filter {
+  constructor(inputId, dataAttribute) {
+    this.elements = document.querySelectorAll(`[${dataAttribute}]`);
+    const inputElement = document.querySelector(`#${inputId}`);
+    inputElement.addEventListener("input", (event) => {
+      handleSearch(event, this.elements);
+    });
+  }
+}
 
-// // Filter-Klasse
-// class Filter {
-//   constructor(inputId, dataAttribute) {
-//     this.elements = document.querySelectorAll(`[${dataAttribute}]`);
-//     const inputElement = document.querySelector(`#${inputId}`);
-//     inputElement.addEventListener("input", (event) => {
-//       handleSearch(event, this.elements);
-//     });
-//   }
-// }
-
-// // Anwendung
-// // Neue Instanz der Filter-Klasse mit ID des Inputs und Attribut der zu filternden Elemente
-// const searchFilter = new Filter("search", "data-caption");
+// Aufruf und Start des Filters
+const searchFilter = new Filter("search", "data-caption");
